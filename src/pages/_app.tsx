ea82d6a29c1fe426f3e5c8cdfabb2b43685ffabe
Gustavo Santos <100292023+gsantos20@users.noreply.github.com>
2023-09-27 00:26:36 -0300

    feat: import boostrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../../styles/globals.scss'
-import { AppProps } from "next/app"
+import React, { useEffect } from 'react'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps){
-    return <Component {...pageProps}/>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
