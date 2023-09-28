a49014712a6925dd214d71e8f106d17fcc0526eb
raio <mmarquesraioane586@gmail.com>
2023-09-27 22:41:25 -0300

    okok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,22 @@
-import React from 'react'
 import Head from 'next/head';
 import styles from '../../styles/Home.module.scss';
+import  { Input } from '../components/ui/Input';
 
-const Login: React.FC = () => {
-  return (
+export default function Home(){
+  return(
     <>
-
-    <Head>
-    <title>Les Pattes - Login</title>
-   </Head>
-
-    <div className='container d-flex justify-content-center align-items-center w-100 h-100'>
-      <div className='card p-5 w-100 h-auto'>
-        <div className='card-body d-grid justify-content-center'>
-          <div className='row mb-4 text-center'>
-            <h2>Cadastre-se</h2>
-            <p>Necessitamos dos seus dados para prosseguir</p>
-          </div>
-
-          <div>
-            <div className='row'>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='&#xf007;   nome' style={{fontFamily: 'Arial, FontAwesome'}} />
-              </div>
-                <div className="col">
-                  <input type="text" className="form-control p-2" placeholder="nome"/>
-                </div>
-            </div>
-
-            <div className='row mt-3'>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='cep' />
-              </div>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='cpf' />
-              </div>
-            </div>
-
-            <div className='row mt-3'>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='e-mail' />
-              </div>
-            </div>
-
-            <div className='row mt-3'>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='senha' />
-              </div>
-            </div>
-
-            <div className='row mt-3'>
-              <div className='col'>
-                <input className='form-control p-2' placeholder='confirmar senha' />
-              </div>
-            </div>
-
-            <div className='row mt-4 justify-content-center'>
-              <button className='btn btn-outline-primary text-dark fw-bold rounded-4 p-2 col-4'>Cadastrar</button>
-            </div>
-          </div>
-
+      <Head>
+      <title>Les Pattes - Login</title>
+      </Head>
+       <div className={styles.containerCenter}>
+        <h1>Boas-vindas de volta!</h1>
+        <h3>Conecte-se novamente!</h3>
+        <div className={styles.login}>
+            <form>
+                <Input placeholder='&#xf007; cpf'/>
+            </form>
         </div>
-      </div>
-    </div>
+        </div>
     </>
   )
 }
-
-
-export default Login
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.scss';
+import { Input } from '@/components/ui/Input';
 
-const Test: React.FC = () => {
+const Login: React.FC = () => {
   return (
+    <>
+
+    <Head>
+    <title>Les Pattes - Login</title>
+   </Head>
+
     <div className='container d-flex justify-content-center align-items-center w-100 h-100'>
       <div className='card p-5 w-100 h-auto'>
         <div className='card-body d-grid justify-content-center'>
           <div className='row mb-4 text-center'>
-            <h2>Cadastre-se</h2>
-            <p>Necessitamos dos seus dados para prosseguir</p>
+            <h1>Cadastre-se</h1>
+            <h2>Necessitamos dos seus dados para prosseguir</h2>
           </div>
 
           <div>
             <div className='row'>
               <div className='col'>
-                <input className='form-control p-2' placeholder='&#xF007;   nome' style={{fontFamily: 'Poppins, FontAwesome'}} />
+                <input className='form-control p-2' placeholder='&#xf007;   nome' style={{fontFamily: 'Poppins, Sans'}} />
               </div>
                 <div className="col">
                   <input type="text" className="form-control p-2" placeholder="nome"/>
@@ -55,8 +64,9 @@ const Test: React.FC = () => {
         </div>
       </div>
     </div>
+    </>
   )
 }
 
 
-export default Test
+export default Login
