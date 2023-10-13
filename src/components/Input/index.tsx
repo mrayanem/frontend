69f1e7988e202b51b76f373f3ea778d4d5e74da9
Gustavo Neris <nerisgs20@gmail.com>
2023-10-13 17:39:13 -0300

    feat: add fontawesome font

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,9 @@
 import { InputHTMLAttributes } from 'react';
-import styles from './styles.scss'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{}
 
 export const Input = ({...rest}: InputProps ) => {
   return(
-    <input className={styles.input} {...rest} />
+    <input {...rest} />
   )
 }
